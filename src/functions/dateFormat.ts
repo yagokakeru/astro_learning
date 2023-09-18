59d13576bf00fe07eaea44f1d863add027297a5e
github yagokakeru <aisubestlike.0316@icloud.com>
2023-09-18 19:16:04 +0900

    maicrocmsの設定

---
+++ src/functions/dateFormat.ts
@@ -0,0 +1,16 @@
+/**
+ * 日付をフォーマットする関数
+ * 
+ * maicroCMSから取得した日付をYYYY.MM.DDにフォーマットする
+ * 
+ * @param date string maicroCMSから取得した日付
+ */
+export function dateFormat(date: string) {
+    const dateFormat =  new Date(date).toLocaleDateString('ja-JP',{
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+	}).split('/').join('.');
+
+    return dateFormat;
+}
