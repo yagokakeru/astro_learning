a7c28f77836ba02843af4054b772870843f13b1a
github yagokakeru <aisubestlike.0316@icloud.com>
2023-09-24 18:54:18 +0900

    netlifyエラー解決

--- src/library/dateformat.ts
+++ src/library/dateformat.ts
@@ -5,12 +5,12 @@
  * 
  * @param date string maicroCMSから取得した日付
  */
-export const dateformat = (date: string) => {
-    const returndate =  new Date(date).toLocaleDateString('ja-JP',{
+export const getDateFormat = (date: string) => {
+    const returnDate =  new Date(date).toLocaleDateString('ja-JP',{
 		year: 'numeric',
 		month: '2-digit',
 		day: '2-digit',
 	}).split('/').join('.');
 
-    return returndate;
+    return returnDate;
 }
